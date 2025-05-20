7498d8ebace9d1660a6334c352e6337267b3818f
Brandon Smith <bwsmith09@gmail.com>
2025-05-20 11:46:42 -0400

    change res.setHeaders to res.set

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/secret', async (req, res) => {
     currency: 'usd',
   });
 
-  res.setHeaders('Access-Control-Allow-Origin', 'https://magnoliacremations.com');
+  res.set('Access-Control-Allow-Origin', 'https://magnoliacremations.com');
   res.json({client_secret: paymentIntent.client_secret});
 });
 
