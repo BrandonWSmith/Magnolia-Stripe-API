c8774aa9f8fc24db5c044562a1c349d3e234c50f
Brandon Smith <bwsmith09@gmail.com>
2025-05-22 17:13:12 -0400

    change res.send to res.json in form data endpoints

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post('/store-form-data', (req, res) => {
   hulkFormData = formData;
 
   res.set('Access-Control-Allow-Origin', 'https://magnoliacremations.com');
-  res.status(201).send("Form data stored on server");
+  res.status(201).json({"message": "Form data stored on server"});
 });
 
 app.get('/get-form-data', (req, res) => {
@@ -114,7 +114,7 @@ app.get('/get-form-data', (req, res) => {
   } else {
     console.log('IP does not match');
     res.set('Access-Control-Allow-Origin', 'https://magnoliacremations.com');
-    res.send("There was an issue retrieving form data");
+    res.json({"message": "There was an issue retrieving form data"});
   }
 });
 
