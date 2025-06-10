075870362d713437c9d41240af6ebb4647170ed3
Brandon Smith <bwsmith09@gmail.com>
2025-06-10 17:00:34 -0400

    minify body in klaviyo-checkout-event endpoint

--- server.js
+++ server.js
@@ -25,41 +25,7 @@ app.post('/klaviyo-checkout-event', async (req, res) => {
       'content-type': 'application/vnd.api+json',
       Authorization: `Klaviyo-API-Key ${process.env.KLAVIYO_SECRET_KEY}`
     },
-    body: `{
-      "data":{
-        "type":"event",
-        "attributes":{
-          "properties":{
-            "$extra:${formData}
-          },
-          "metric":{
-            "data":{
-              "type":"metric",
-              "attributes":{
-                "name":"Form Data"
-              }
-            }
-          },
-          "profile":{
-            "data":{
-              "type":"profile",
-              "attributes":{
-                "location":{
-                  "address1":${formData.contact_street_address},
-                  "city":${formData.contact_city},
-                  "country":"United States",
-                  "region":${formData.contact_state},
-                  "zip":${formData.contact_zip_code}
-                },
-                "email":${formData.contact_email},"phone_number":${formData.contact_phone},
-                "first_name":${formData.contact_first_name},
-                "last_name":${formData.contact_last_name}
-              }
-            }
-          }
-        }
-      }
-    }`
+    body: `{"data":{"type":"event","attributes":{"properties":{"$extra:${formData}},"metric":{"data":{"type":"metric","attributes":{"name":"Form Data"}}},"profile":{"data":{"type":"profile","attributes":{"location":{"address1":${formData.contact_street_address},"city":${formData.contact_city},"country":"United States","region":${formData.contact_state},"zip":${formData.contact_zip_code}},"email":${formData.contact_email},"phone_number":${formData.contact_phone},"first_name":${formData.contact_first_name},"last_name":${formData.contact_last_name}}}}}}}`
   };
 
   fetch(url, options)
