bc3b581ba73cea795f3f0882f2039696d30e3d44
Brandon Smith <bwsmith09@gmail.com>
2025-06-13 17:41:25 -0400

    add endpoint for calculator contact

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 11000;
 require('@shopify/shopify-api/adapters/node');
 const { Session, shopifyApi, LATEST_API_VERSION } = require('@shopify/shopify-api');
-const Klaviyo = require('klaviyo-node');
+const nodemailer = require('nodemailer');
 const stripe = require('stripe')(process.env.STRIPE_SERVER_KEY)
 
 app.use(express.json());
@@ -13,6 +13,75 @@ app.use(cors({
   origin: '*',
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+app.post('/calculator-contact', (req, res) => {
+  const { formData } = req.body;
+  console.log(formData);
+  const emailBody = `
+    <p>Magnolia's Essential Cremation Package</p>
+    <b>Cremation Service Package - $895</b>
+    <br>
+    <p>Choose the Perfect Urn for Your Loved One</p>
+    <b>${formData.urn_price}</b>
+    <br>
+    <p>Would you like to personalize the urn with a special engraving?</p>
+    <b>${formData.urn_engraving ? "Yes" : "No"}</b>
+    <br>
+    <p>Would you like to schedule a "Private Family Viewing?</p>
+    <b>${formData.private_family_viewing_total > 0 ? "Yes" : "No"}</b>
+    <br>
+    <p>Please enter the address (or City, State) for the pickup location of your loved one to calculate any additional transporation fees:</p>
+    <b>${formData.contact_street_address}, ${formData.contact_city}, ${formData.contact_state} ${formData.contact_zip_code}</b>
+    <br>
+    <p>Additional transportation fee from pickup location:</p>
+    <b>${formData.transfer_fee}</b>
+    <br>
+    <p>How many death certificates do you need?</p>
+    <b>${formData.death_certificates_quantity}</b>
+    <br>
+    <p>Would you like us to use USPS's cremains service to safely ship your loved one to you?</p>
+    <b>${formData.shipping > 0 ? "Yes" : "No, I'll pick up my loved one."}</b>
+    <br>
+    <p>TOTAL PACKAGE PRICE</p>
+    <b>${formData.total_price}</b>
+    <br>
+    <p>Name:</p>
+    <b>${formData.name}</b>
+    <br>
+    <p>Phone:</p>
+    <b>${formData.phone}</b>
+    <br>
+    <p>How Would You Prefer to Communicate?</p>
+    <b>${formData.contact_type}</b>
+    <br>
+    <p>Preferred Time?</p>
+    <b>${formData.contact_time}</b>
+  `;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'brandon@magnoliacremations.com',
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: 'brandon@magnoliacremations.com',
+    to: 'brandon@magnoliacremations.com',
+    subject: 'Client Requesting Assistance',
+    html: emailBody
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.json({data: error});
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json({data: info});
+    }
+  });
+});
 
 app.post('/klaviyo-checkout-event', async (req, res) => {
   const { formData } = req.body;
