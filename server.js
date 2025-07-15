6184c92867756399dbd4121a803d3a11e274dd5e
Brandon Smith <bwsmith09@gmail.com>
2025-07-15 09:22:41 -0400

    update(create-payment-intent): change metadata formData to JSON string format

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ app.post('/create-payment-intent', async (req, res) => {
       },
     },
     description: `Purchaser: ${formData.contact_first_name} ${formData.contact_last_name}`,
-    metadata: {formData: JSON.parse(formData)},
+    metadata: {formData: JSON.stringify(formData)},
   });
 
   res.json({client_secret: paymentIntent.client_secret});
