023157b9eab70d9c7a0996c1cb8f4e2114703b56
Brandon Smith <bwsmith09@gmail.com>
2025-07-15 12:39:10 -0400

    update(webhook): log errors from webhook response for better error handling

--- server.js
+++ server.js
@@ -489,7 +489,7 @@ app.post('/webhook', express.raw({type: 'application/json'}), async (req, res) =
       body: JSON.stringify({queryString: queryString, variables: variables}),
     })
     .then(response => response.json())
-    .then(data => console.log(data.data.body));
+    .then(data => console.log(data.data.body.errors));
   } else if (event.type === 'payment_intent.payment_failed') {
     const paymentIntent = event.data.object;
     console.log(`PaymentIntent failed! ID: ${paymentIntent.id}`);
