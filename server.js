0a9c2c709c4db36f63008c4cb71fbb506bd05e17
Brandon Smith <bwsmith09@gmail.com>
2025-08-28 13:30:06 -0400

    update(server): add customer selection option to generate-discount-code response

--- server.js
+++ server.js
@@ -761,7 +761,7 @@ app.get('/generate-discount-code', async (req, res) => {
         "customerGets": {
           "value": {
             "discountAmount": {
-              "amount": 1200.00,
+              "amount": "1200.00",
               "appliesOnEachItem": false
             }
           },
@@ -769,7 +769,10 @@ app.get('/generate-discount-code', async (req, res) => {
             "all": true
           }
         },
-        "usageLimit": 1
+        "usageLimit": 1,
+        "customerSelection": {
+          "all": true
+        }
       }
 
       await fetch('https://magnolia-api.onrender.com/shopify-admin-api',
