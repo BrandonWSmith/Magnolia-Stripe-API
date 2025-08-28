888c9d1614eb368146f5f709cdbb281278554726
Brandon Smith <bwsmith09@gmail.com>
2025-08-28 16:23:36 -0400

    update(server): fix discount code existence check in generate-discount-code endpoint

--- server.js
+++ server.js
@@ -715,7 +715,7 @@ app.get('/generate-discount-code', async (req, res) => {
   )
   .then(response => response.json())
   .then(async data => {
-    discountCodeExists = typeof data.data.codeDiscountNodeByCode != 'undefined';
+    discountCodeExists = typeof data.data.data.codeDiscountNodeByCode != 'undefined';
 
     if (!discountCodeExists) {
       const createDiscountCodeQueryString = `mutation CreateDiscountCode($basicCodeDiscount: DiscountCodeBasicInput!) {
